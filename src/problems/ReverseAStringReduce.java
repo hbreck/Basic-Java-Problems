491f99c88cd4fdac989f5f8470438db97255194c
Herman Breckenridge <hbrecken@pnw.edu>
2020-11-15 00:23:18 -0600

    Reverse a String using Reduce stream

---
+++ src/problems/ReverseAStringReduce.java
@@ -0,0 +1,26 @@
+package problems;
+
+import java.util.stream.Stream;
+
+public class ReverseAStringReduce {
+    public static void main(String[] args) {
+
+        // Assign string type variable to the problem string
+        String problem = "Herman";
+
+        // Assign string typ variable to solution string
+        String result = solution(problem);
+
+        //Print solution result
+        System.out.println(result);
+    }
+
+    // This function uses a stream to reverse to the input string.
+    // This uses reduce to produce the necessary
+    // swaps to print out the reversed string.
+    public static String solution(String word) {
+        return Stream.of(word.split(""))
+                .reduce("", (original, reverse) ->
+                        reverse + original);
+    }
+}
