862d8ab8e2a30276f866f9ce752f9ca3773c0c2b
Herman Breckenridge <hbrecken@pnw.edu>
2020-11-15 17:51:57 -0600

    Decrement an integer by one

---
+++ src/problems/Decrement.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class Decrement {
+    public static void main(String[] args) {
+        // Create int type variable and call it problem
+        int problem = 5;
+
+        // Print solution function
+        System.out.println(solution(problem));
+
+    }
+
+    // This function takes an integer and decrements it by one
+    public static int solution(int x){
+        return x - 1;
+    }
+}
