598fb8157d60801026c4392e95282bf0437e1bd7
Herman Breckenridge <hbrecken@pnw.edu>
2020-11-15 18:09:15 -0600

    Squaring an integer

---
+++ src/problems/SquareANumber.java
@@ -0,0 +1,16 @@
+package problems;
+
+public class SquareANumber {
+    public static void main(String[] args) {
+        // Create variable to be used in solution function
+        int x = 5;
+
+        // Print solution function
+        System.out.println(solution(x));
+    }
+
+    // This function takes an integer and returns the square of that integer
+    public static int solution(int x){
+        return x * x;
+    }
+}
