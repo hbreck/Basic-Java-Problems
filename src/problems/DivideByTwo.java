fe1ca61bba747d3c34650b52b3a1561a2be5ee37
Herman Breckenridge <hbrecken@pnw.edu>
2020-11-15 18:26:25 -0600

    Divide an integer by two

---
+++ src/problems/DivideByTwo.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class DivideByTwo {
+    public static void main(String[] args) {
+        // Create a variable to be used in the solution function
+        int x = 5;
+
+        // Print solution function
+        System.out.println(solution(x));
+
+    }
+
+    // This function takes an integer and returns a double divided by two
+    public static double solution(int x){
+        return (double)x  / 2;
+    }
+}
