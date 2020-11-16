4bd41afe15ae32e3c5a3cec343b46b48f3440395
Herman Breckenridge <hbrecken@pnw.edu>
2020-11-15 22:16:11 -0600

    Comparing three booleans to see if they are all true

--- src/problems/TripleShot.java
+++ src/problems/TripleShot.java
@@ -2,15 +2,18 @@ package problems;
 
 public class TripleShot {
     public static void main(String[] args) {
+        // Create three booleans for the solution function
         boolean x = true;
         boolean y = true;
         boolean z = true;
 
+        // Print solution function
         solution(x, y, z);
 
 
     }
 
+    // This function checks if all three booleans being passed are true.
     public static boolean solution(boolean x, boolean y, boolean z){
         if(x == true && y == true && z == true){
             System.out.println("These conditions are all true");
---
+++ src/problems/TripleShotAgain.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class TripleShotAgain {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean solution(boolean x, boolean y, boolean z){
+        if(x == true || y == true || z == true)
+            return true;
+        return false;
+    }
+}
